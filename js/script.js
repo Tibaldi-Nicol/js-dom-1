f78064c3ac6b441a02a7e7786ce8865e75f12c6f
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-03-25 14:41:09 +0100

    [UPD]Add-switch

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//cerco gli elementi che mi servono con document.getElementById che mi restituisce un oggetto usando l'id dell'elemento 
+//e li salvo in variabili
+
+
+const lampadina = document.getElementById('lampadina');
+const interruttore = document.getElementById('interruttore');
